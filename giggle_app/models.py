f32fdcc5a9cadbb3e4367c6181e15fddc12b102e
Abhinav Mathur <Abhinav Mathur@CRICKABHI.(none)>
2015-01-24 13:46:26 +0530

    Created Models

--- giggle_app/models.py
+++ giggle_app/models.py
@@ -1,3 +1,23 @@
-from django.db import models
-
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+import hashlib
+ 
+ 
+class Ribbit(models.Model):
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_now=True, blank=True)
+ 
+ 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+ 
+    def gravatar_url(self):
+        return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
+ 
+ 
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+
