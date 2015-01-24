37878a606a2d0f405a145991bb6b8f3e81dc3d73
crickabhi <abhinav10503858@gmail.com>
2015-01-24 18:23:21 +0530

    Update settings.py

--- giggle/settings.py
+++ giggle/settings.py
@@ -16,10 +16,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ribbit',                      # Or path to database file if using sqlite3.
-        'USER': 'username',                      # Not used with sqlite3.
-        'PASSWORD': 'password',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'database.db'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -159,7 +159,3 @@ LOGGING = {
         },
     }
 }
-
-import dj_database_url
- 
-DATABASES['default'] = dj_database_url.config()
