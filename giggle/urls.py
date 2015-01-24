686d6a08298b63672013749f4bcd24d651fb0b80
Abhinav Mathur <Abhinav Mathur@CRICKABHI.(none)>
2015-01-24 13:45:00 +0530

    Created templates

--- giggle/urls.py
+++ giggle/urls.py
@@ -6,8 +6,17 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'giggle.views.home', name='home'),
-    # url(r'^giggle/', include('giggle.foo.urls')),
+    url(r'^$', 'ribbit_app.views.index'), # root
+    url(r'^login$', 'ribbit_app.views.login_view'), # login
+    url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
+    url(r'^signup$', 'ribbit_app.views.signup'), # signup
+	url(r'^ribbits$', 'ribbit_app.views.public'), # public ribbits
+    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit
+	url(r'^users/$', 'ribbit_app.views.users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
+    url(r'^follow$', 'ribbit_app.views.follow'),
+    # url(r'^$', 'ribbit.views.home', name='home'),
+    # url(r'^ribbit/', include('ribbit.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
