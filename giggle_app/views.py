4a923e5783bacb690c45ce08e6e4fa0cef14eb20
crickabhi <abhinav10503858@gmail.com>
2015-01-24 18:39:17 +0530

    Update views.py

--- giggle_app/views.py
+++ giggle_app/views.py
@@ -84,7 +84,7 @@ def public(request, ribbit_form=None):
     ribbits = Ribbit.objects.reverse()[:10]
     return render(request,
                   'public.html',
-                  {'ribbit_form': ribbit_form, 'next_url': '/ribbits',
+                  {'ribbit_form': ribbit_form, 'next_url': '/giggles',
                    'ribbits': ribbits, 'username': request.user.username})
 
  
